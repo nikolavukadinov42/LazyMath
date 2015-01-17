1edd49bfee043d1cfe475f4e523596e1f6a47025
nikolavukadinov42 <nikola.vukadinov42@gmail.com>
2015-01-17 14:21:05 +0100

    izbrisao tvoj tree

--- desktop/src/ftn/sc/lazymath/ocr/math/formulatree/DefaultNode.java
+++ desktop/src/ftn/sc/lazymath/ocr/math/formulatree/DefaultNode.java
@@ -4,70 +4,91 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ftn.sc.lazymath.ocr.imageprocessing.RasterRegion;
-import ftn.sc.lazymath.ocr.math.Symbol;
 
 /**
  * Created by nikola42 on 12/29/2014.
  */
 public class DefaultNode extends AbstractNode {
 
-    public DefaultNode(RasterRegion region) {
-        this.region = region;
+	protected List<AbstractNode> exponent = new ArrayList<AbstractNode>();
 
-        this.exponent = new ArrayList<>();
-    }
+	public DefaultNode(RasterRegion region) {
+		this.region = region;
 
-    @Override
-    public String getCharacters() {
-        // karakter + eksponent
-        return null;
-    }
+		this.exponent = new ArrayList<>();
+	}
 
-    @Override
-    public List<RasterRegion> getRasterRegions() {
-        List<RasterRegion> ret = new ArrayList<>();
+	@Override
+	public String getCharacters() {
+		// karakter + eksponent
+		return null;
+	}
 
-        ret.add(region);
+	@Override
+	public List<RasterRegion> getRasterRegions() {
+		List<RasterRegion> ret = new ArrayList<>();
 
-        for (AbstractNode node : exponent){
-            ret.addAll(node.getRasterRegions());
-        }
+		ret.add(this.region);
 
-        return ret;
-    }
+		for (AbstractNode node : this.exponent) {
+			ret.addAll(node.getRasterRegions());
+		}
 
-    
-    @Override
-    public String toString() {
-    	StringBuilder sb = new StringBuilder();
-    	sb.append(region.tag);
-    	if (exponent.size() > 0) {
-    		sb.append("^");
-    	}
-    	for (AbstractNode abstractNode : exponent) {
+		return ret;
+	}
+
+	public AbstractNode getExponent(RasterRegion region) {
+		for (AbstractNode abstractNode : this.exponent) {
+			if (abstractNode.region == region) {
+				return abstractNode;
+			}
+		}
+		return null;
+	}
+
+	public void setExponent(List<AbstractNode> exponent) {
+		this.exponent = exponent;
+	}
+
+	public void removeExponents(List<AbstractNode> exponents) {
+		this.exponent.removeAll(exponents);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.region.tag);
+		if (this.exponent.size() > 0) {
+			sb.append("^");
+		}
+		for (AbstractNode abstractNode : this.exponent) {
 			sb.append(abstractNode);
 		}
-    	return sb.toString();
-    }
-    
-    @Override
+		return sb.toString();
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-		if (obj == null)
+		if (obj == null) {
 			return false;
-		if (obj == this)
+		}
+		if (obj == this) {
 			return true;
-		if (!(obj instanceof DefaultNode))
+		}
+		if (!(obj instanceof DefaultNode)) {
 			return false;
+		}
 		DefaultNode node = (DefaultNode) obj;
-		if(node.getRasterRegion().equals(this.region))
+		if (node.getRasterRegion().equals(this.region)) {
 			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public int hashCode() {
 		// TODO Auto-generated method stub
-		return region.hashCode();
+		return this.region.hashCode();
 	}
-	
+
 }
